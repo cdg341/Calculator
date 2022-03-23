5c6d23a3d5b338dc4d0a4ae2121a456a4d115fb8
Cameron <cdg341@gmail.com>
2022-03-23 02:23:13 -0500

    Have numbers show in the display when clicked

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const clear = document.getElementById("erase");
 const label = document.getElementById("output");
+const numberButtons = document.querySelectorAll(".numbers");
+const equals = document.getElementById("equals");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const num1 = e.target;
+
+    if (label.textContent === "0") {
+      label.textContent = num1.textContent;
+    } else {
+      label.textContent += num1.textContent;
+    }
+  });
+});
 
 clear.addEventListener("click", clearDisplay);
-
+function clearDisplay() {
+  label.textContent = 0;
+}
+/*
 function add(a, b) {
   return a + b;
 }
@@ -19,19 +36,41 @@ function divide(a, b) {
   return a / b;
 }
 
+function appendNumber(number) {
+  if (label.textContent === "0") {
+    clearDisplay();
+  }
+}
+
 function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
       return add(a, b);
     case "-":
-      return subtract(a - b);
-    case "*":
-      return multiply(a * b);
-    case "/":
-      return divide(a / b);
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
   }
 }
 
-function clearDisplay() {
-  label.textContent = 0;
+
+
+function numbersClicked(number) {
+  //let label = label.textContent;
+  switch (number) {
+    case 1:
+      return (label.textContent = 1);
+    case 2:
+      return (label.textContent = 2);
+    case 3:
+      return (label.textContent = 3);
+  }
 }
+ */
